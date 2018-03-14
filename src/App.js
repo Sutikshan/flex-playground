52d68489cd57578631dd90a4eb8393da83531719
Anand Dubey <anand.dubey@FacilitateDigital.local>
2018-03-14 16:45:51 +1100

    add menu buttons

--- src/App.js
+++ src/App.js
@@ -8,8 +8,15 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Flex Playground</h1>
+          <div className="Menu-bar">
+            <a href="#" className="button button-01">
+              FlexContainer
+            </a>
+            <a href="#" className="button button-01">
+              Flexibility
+            </a>
+          </div>
         </header>
         <p className="App-intro">
           Please edit flex-box properties to see the effects.
